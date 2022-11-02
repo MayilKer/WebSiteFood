fe61497b028b57c99f8121b86eab087ecd7cac79
MayilZion <kerimov0410@gmail.com>
2022-11-02 20:19:39 +0400

    FixeNegativValueOnTime

---
+++ js/script.js
@@ -0,0 +1,102 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent(){
+        tabsContent.forEach(elem => {
+            elem.classList.add('hide');
+            elem.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(elem => {
+            elem.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0 ){
+        tabsContent[i].classList.add('show','fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    tabsParent.addEventListener('click', (event) => {
+        let target = event.target;
+        tabs.forEach((elem, i) => {
+            if(elem == target){
+                hideTabContent();
+                showTabContent(i);
+            }
+        });
+    });
+
+
+    hideTabContent();
+    showTabContent();
+
+
+
+    const deadLine = '2023-11-02';
+
+    function getTimeRemaining(endtime){
+
+        let days, seconds, minutes, hours;
+
+        const t = Date.parse(deadLine) - Date.parse(new Date());
+
+        if(t <= 0){
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        }else{
+            days = Math.floor( (t/(1000*60*60*24)) );
+            seconds = Math.floor( (t/1000) % 60 );
+            minutes = Math.floor( (t/1000/60) % 60 );
+            hours = Math.floor( (t/(1000*60*60) % 24) );
+        }
+        
+        return{
+            total: t,
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds
+        };
+    }
+
+    function getZeroTime(num){
+        if(num >= 0 && num < 10){
+            return `0${num}`;
+        }else{
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime){
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+            
+        function updateClock(){
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZeroTime(t.days);
+            hours.innerHTML = getZeroTime(t.hours);
+            minutes.innerHTML = getZeroTime(t.minutes);
+            seconds.innerHTML = getZeroTime(t.seconds);
+
+            if(t.total <= 0){
+                clearInterval(timeInterval);    
+            }
+        }
+    }
+
+    setClock('.timer',deadLine);
+
+});
