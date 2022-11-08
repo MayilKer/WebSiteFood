0b609ccd391c237e227aba2d0209539361a530fa
Mayil Kerimov <kerimov0410@gmail.com>
2022-11-08 19:13:01 +0400

    addedClass

--- js/script.js
+++ js/script.js
@@ -151,4 +151,64 @@ window.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('scroll', modalWhileScroll);
 
+    class MenuCard {
+        constructor(src, alt, price, title, descr, parentSelector){
+            this.src = src;
+            this.alt = alt;
+            this.price = price;
+            this.title = title;
+            this.descr = descr;
+            this.parentSelector = document.querySelector(parentSelector);
+            this.transfer= 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            return this.price *= this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+            <div class="menu__item">
+                    <img src="${this.src}" alt="${this.alt}">
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parentSelector.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        9,
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        14,
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        21,
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        ".menu .container"
+    ).render();
 });
