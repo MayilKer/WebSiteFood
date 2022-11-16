2e4d8d0b0909172c61a20d410865fa174c015354
MayilZion <kerimov0410@gmail.com>
2022-11-16 17:55:55 +0400

    ServerFirstCommit

--- js/script.js
+++ js/script.js
@@ -220,4 +220,24 @@ window.addEventListener('DOMContentLoaded', () => {
         ".menu .container",
         "menu__item"
     ).render();
+
+    // Forms
+
+    const forms = document.querySelectorAll('form');
+
+    function postData(form){
+        form.addEventListener('submit', (e) => {
+           e.preventDefault(); 
+
+           const request = new XMLHttpRequest();
+           request.open('POST', 'server.php');
+           request.setRequestHeader('Content-type', 'multipart/form-data');
+
+           const formData = new FormData();
+           form.append(form);
+
+           request.send(formData);
+        });
+    }
+
 });
