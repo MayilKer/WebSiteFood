65ad8eea0afe515496fe55b3c271abf81c7cbbdc
Mayil Kerimov <kerimov0410@gmail.com>
2022-12-05 11:56:13 +0400

    EndCalculator

--- js/script.js
+++ js/script.js
@@ -507,7 +507,40 @@ window.addEventListener('DOMContentLoaded', () => {
     // Calculator
 
     const result = document.querySelector('.calculating__result span');
-    let sex = 'female', height, weight, age, ratio = 1.375;
+    let sex, height, weight, age, ratio;
+
+
+    if(localStorage.getItem('sex')){
+        sex = localStorage.getItem('sex');
+    }else{
+        sex = 'female';
+        localStorage.setItem('sex', 'female');
+    }
+
+    if(localStorage.getItem('ratio')){
+        ratio = localStorage.getItem('ratio');
+    }else{
+        ratio = 1.375;
+        localStorage.setItem('ratio', 1.375);
+    }
+
+    getInitialLocalStorage('#gender div', 'calculating__choose-item_active');
+    getInitialLocalStorage('.calculating__choose_big div', 'calculating__choose-item_active');
+
+    function getInitialLocalStorage(selector, activeClass){
+        const elements = document.querySelectorAll(selector);
+
+        elements.forEach(element => {
+            element.classList.remove(activeClass);
+            if(element.getAttribute('id') === localStorage.getItem('sex')){
+                element.classList.add(activeClass);
+            }
+            if(element.getAttribute('data-ratio') === localStorage.getItem('ratio')){
+                element.classList.add(activeClass);
+            }
+        });
+    }
+    
     
     function calcFunc(){
         if(!sex || !height || !weight || !age || !ratio){
@@ -530,9 +563,11 @@ window.addEventListener('DOMContentLoaded', () => {
         elements.forEach(elem => {
             elem.addEventListener('click', (e) => {
                 if(e.target.getAttribute('data-ratio')){
-                    ratio = +e.target.getAttribute('data-ratio');   
+                    ratio = +e.target.getAttribute('data-ratio');
+                    localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));   
                 } else {
                     sex = e.target.getAttribute('id');
+                    localStorage.setItem('sex', e.target.getAttribute('id'));
                 }
     
                 elements.forEach(elem => {
@@ -552,6 +587,13 @@ window.addEventListener('DOMContentLoaded', () => {
     function dynamicInputCheck(selector){
         let input = document.querySelector(selector);
         input.addEventListener('input', () => {
+
+            if(input.value.match(/\D/g)){
+                input.style.border = '1px solid red';
+            }else{
+                input.style.border = 'none';
+            }
+
             switch(input.getAttribute('id')) {
                 case "height":
                     height = +input.value;
